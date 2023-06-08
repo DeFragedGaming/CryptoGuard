fc98ecdcf51e032d7d3c337ad9198d893ce9ab1a
DeFragedGaming <mattyhammel17@gmail.com>
2023-06-08 04:08:05 -0700

    getting ready to remove backend from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Posts from './components/Posts';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <h1>Welcome to Your App</h1>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/posts">Posts</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/about" component={About} />
+        </Routes>
+      </main>
+
+      <footer>
+        &copy; 2023 Your App. All rights reserved.
+      </footer>
+    </Router>
   );
 }
 
